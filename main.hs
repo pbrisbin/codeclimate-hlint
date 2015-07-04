234c5116a092431eb4e3e7ef77f6211b8c5e6879
patrick brisbin <pbrisbin@gmail.com>
2015-07-03 22:47:23 -0400

    Simplify main.hs

--- main.hs
+++ main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import CC.Analyze
@@ -5,6 +6,7 @@ import CC.Config
 import CC.Result
 
 import Data.Aeson (encode)
+import Data.Monoid ((<>))
 import System.FilePath.Glob (compile, globDir)
 
 import qualified Data.ByteString.Lazy as BL
@@ -12,18 +14,14 @@ import qualified Data.ByteString.Lazy as BL
 main :: IO ()
 main = do
     config <- loadConfig "/config.json"
-
     let included = not . (`elem` configExcludes config)
 
-    files <- hsFiles
-    results <- analyzeFiles $ filter included files
-
-    mapM_ printResult results
+    mapM_ printResult
+        =<< analyzeFiles
+        =<< filter included <$> hsFiles
 
 hsFiles :: IO [FilePath]
 hsFiles = concat . fst <$> globDir [compile "**/*.hs"] "."
 
 printResult :: Result -> IO ()
-printResult result = do
-    BL.putStr $ encode result
-    putChar '\0'
+printResult = BL.putStr . (<> "\0") . encode
