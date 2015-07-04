875f92a593857119fc0c6557acd1b3904d5766e2
patrick brisbin <pbrisbin@gmail.com>
2015-07-03 22:33:14 -0400

    Implement exclude_paths loading

--- src/CC/Config.hs
+++ src/CC/Config.hs
@@ -1,11 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 module CC.Config
     ( Config(..)
     , loadConfig
     ) where
 
-data Config = Config
-    { configExcludes :: [FilePath]
-    }
+import Data.Aeson
+import Data.Maybe (fromMaybe)
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.Encoding (encodeUtf8)
+import System.Directory
+
+import qualified Data.Text.Lazy.IO as T
+
+data Config = Config { configExcludes :: [FilePath] }
+
+instance FromJSON Config where
+    parseJSON = withObject "Config" $ \o -> do
+        Config <$> o .: "exclude_paths"
 
 loadConfig :: FilePath -> IO Config
-loadConfig _ = return $ Config []
+loadConfig fp = do
+    exists <- doesFileExist fp
+    mconfig <- if exists
+        then decodeText <$> T.readFile fp
+        else return Nothing
+
+    return $ fromMaybe (Config []) mconfig
+
+decodeText :: FromJSON a => Text -> Maybe a
+decodeText = decode . encodeUtf8
